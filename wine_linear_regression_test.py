7e31737a5c20443a3e5766ce81d50cb8f91844ee
pkaferle <62647072+pkaferle@users.noreply.github.com>
2023-01-31 07:08:33 +0100

    Update wine_linear_regression_test.py

--- wine_linear_regression_test.py
+++ wine_linear_regression_test.py
@@ -18,9 +18,9 @@ import sys
 
 import pandas as pd
 import numpy as np
-from scikit-learn.metrics import mean_squared_error, mean_absolute_error, r2_score
-from scikit-learn.model_selection import train_test_split
-from scikit-learn.linear_model import ElasticNet
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import ElasticNet
 
 import logging
 
